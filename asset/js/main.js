d65d496244cb08a82b43de1b1017643d3905365d
mehebbetbabayeva <mhbbtbabaeva97@gmail.com>
2023-01-28 16:08:03 +0400

    finish button

---
+++ asset/js/main.js
@@ -0,0 +1,12 @@
+document.querySelector("#header-menu").addEventListener("click" , ()=>{
+   let mobileMenu= document.querySelector(".deactive");
+   mobileMenu.classList.remove("deactive");
+   mobileMenu.classList.add("active");
+});
+document.querySelector(".backbtn").addEventListener("click" , ()=>{
+    let mobileMenu=document.querySelector(".header-menu");
+    mobileMenu.classList.remove("active");
+    mobileMenu.classList.add("deactive")
+});
+
+
